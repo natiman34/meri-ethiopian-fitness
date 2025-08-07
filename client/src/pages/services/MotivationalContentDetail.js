5f95ba29f49d6fc32239d6c32a9c4a2c51135d45
natiman34 <nati737313@gmail.com>
2025-08-06 21:30:42 -0700

    Delete client/src/pages/services/MotivationalContentDetail.js

--- client/src/pages/services/MotivationalContentDetail.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { useParams } from "react-router-dom";
-const MotivationalContentDetail = () => {
-    const { id } = useParams();
-    return (_jsx("div", { className: "pt-24 pb-16", children: _jsxs("div", { className: "container mx-auto px-4", children: [_jsx("h1", { className: "text-3xl font-bold", children: "Motivational Content Detail" }), _jsxs("p", { className: "mt-4", children: ["Viewing motivational content with ID: ", id] })] }) }));
-};
-export default MotivationalContentDetail;
